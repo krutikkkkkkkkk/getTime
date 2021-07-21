14b516796887d2dbc639b2b5134b753d3e19f48f
Krutik Raut <58563133+reboot13-git@users.noreply.github.com>
2021-07-21 16:43:23 +0530

    Added route /api to get IST time

--- app.js
+++ app.js
@@ -34,6 +34,29 @@ app.get('/api/timezone=:tz(*)', (req, res)=> {
     
 })
 
+app.get('/api', (req, res)=> {
+    try{
+        let d = new Date().toLocaleString('en-US', {timeZone: 'Asia/Kolkata'});
+        d = d.split(', ')
+        let object = {
+            result: true,
+            date : d[0],
+            time : d[1]
+        }
+        res.send(object);
+    }
+    
+    catch(error){
+        let object = {
+            result: false,
+            message: error.message
+        }
+        res.send(object)
+    }
+  
+    
+})
+
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
